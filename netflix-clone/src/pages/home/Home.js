cf3c4c65ccc55965b55d6f89c72bcc31b9e0ba5b
rita <ritaayalew6.com>
2024-09-14 21:00:04 +0300

    banner

--- netflix-clone/src/pages/home/Home.js
+++ netflix-clone/src/pages/home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
+import Banner from '../../components/Banner/Banner';
 
 function Home() {
   return (
     <div>
         <Header/>
+        <Banner/>
         <Footer/>
-        <p>hello</p>
-       
     </div>
   )
 }
